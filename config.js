68d2c7c9fe051cc1970ff58dd728803c5bf72468
Takitori <nhatovernight1204@gmail.com>
2021-11-24 21:00:56 +0700

    Added new files

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hello Ty ! 🖐🖐🖐',
+    introDesc: `Quang có điều này muốn hỏi Ty, Ty nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Etouuuu?',
+    desc: 'Thật sự thì.......',
+    btnYes: 'Click vào đây nhé💖💖💖',
+    btnNo: '💔  💔 ',
+    question:'hmmmm😊😊',
+    btnReply: 'CLick vào đây nè! 💌💌',
+    reply: 'Âu nâu😊😊',
+    mess: 'Hmm',
+    messDesc: '💬💬 Nhắn tin cho Quang nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+
+}
