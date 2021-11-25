9227d9eee31cb14f8d08bffd0fb4fae6cf7e73de
Takitori <nhatovernight1204@gmail.com>
2021-11-25 21:46:14 +0700

    added new files

--- js/main.js
+++ js/main.js
@@ -10,6 +10,15 @@ $(document).ready(function() {
     }, 600);
 })
 
+
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (!isChrome){
+    $('#iframeAudio').remove()
+}
+else {
+    $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+}
+
 function init(){
     $('#title').text(CONFIG.title)
     $('#desc').text(CONFIG.desc)
